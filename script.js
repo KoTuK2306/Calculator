4453efe493aa39622763b1d33d688f6710e5b088
KoTuK2306 <dmitriy_aleshin_00@mail.ru>
2022-01-21 18:41:36 +0300

    refactor: code impovements

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-const buttons = document.getElementById("buttonsBlock");
-const result = document.getElementById("result");
+const buttons = document.getElementsByClassName("buttonsBlock");
+const result = document.getElementsByTagName("p");
 
 buttonClick = (event) => {
-    if (event.target.id == "clean") {
-        result.innerHTML = 0;
-        return;
-    }
-    document.getElementById("result").innerHTML = event.target.innerHTML;
+  if (event.target.id === "clean") {
+    result.innerHTML = 0;
+    return;
+  }
+  document.getElementById("result").innerHTML = event.target.innerHTML;
 };
 
-buttons.addEventListener("click", buttonClick)
-    
+buttons.addEventListener("click", buttonClick);
