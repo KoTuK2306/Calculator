04c3d1b28141a81da5696fa31e4a27e7d8440a6e
KoTuK2306 <dmitriy_aleshin_00@mail.ru>
2022-01-21 19:44:23 +0300

    refactor: syntax improvements

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const [buttonsContainer] = document.getElementsByClassName("buttonsBlock");
 const [result] = document.getElementsByClassName("result");
 
-let buttonClick = (event) => {
+const buttonClick = (event) => {
   if (event.target.id === "clean") {
     result.innerHTML = 0;
     return;
   }
+
   result.innerHTML = event.target.innerHTML;
 };
 
