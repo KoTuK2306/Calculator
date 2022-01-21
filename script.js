047e39fef87cf453fd09b8ec01d3df0a56336f39
KoTuK2306 <dmitriy_aleshin_00@mail.ru>
2022-01-21 16:35:44 +0300

    feat: numbers typing and backspace

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 const [buttonsContainer] = document.getElementsByClassName("buttonsBlock");
 const [result] = document.getElementsByClassName("result");
+let calculating = [];
 
 const buttonClick = (event) => {
-  if (event.target.id === "clean") {
+  if (event.target.id == "clean") {
     result.innerHTML = 0;
+    calculating = [];
     return;
+  } else if (event.target.id == "backspace") {
+    calculating.pop();
+    result.innerHTML = calculating.join("");
+    return;
+  } else {
+    calculating.push(event.target.innerHTML);
+    result.innerHTML = calculating.join("");
   }
-
-  result.innerHTML = event.target.innerHTML;
 };
 
 buttonsContainer.addEventListener("click", buttonClick);
