57057f84a03ca1d8a2db4fc5da414e77bf5c92d4
KoTuK2306 <dmitriy_aleshin_00@mail.ru>
2022-01-22 20:29:06 +0300

    feat: sizing text and max length

--- script.js
+++ script.js
@@ -9,19 +9,23 @@ const buttonClick = (event) => {
     case operationButtons.clean:
       result.innerHTML = 0;
       calculating = [];
+      sizingText();
       break;
     case operationButtons.backspace:
       calculating.pop();
       if (calculating.length === 0) {
         result.innerHTML = 0;
+        sizingText();
         return;
       }
       result.innerHTML = calculating.join("");
+      sizingText();
       break;
     case operationButtons.square:
       const number = calculating.join("");
       result.innerHTML = Math.pow(number, 2);
       calculating = [];
+      sizingText();
       break;
     case operationButtons.partOfAWhole:
       const denominator = calculating.join("");
@@ -29,11 +33,21 @@ const buttonClick = (event) => {
         ? (result.innerHTML = operationButtons.uncertainty)
         : (result.innerHTML = 1 / denominator);
       calculating = [];
+      sizingText();
       break;
     default:
       calculating.push(event.target.innerHTML);
       result.innerHTML = calculating.join("");
+      sizingText();
   }
 };
 
 buttonsContainer.addEventListener("click", buttonClick);
+
+const sizingText = () => {
+  if ((calculating.length > 15) & (calculating.length <= 38)) {
+    document.getElementsByClassName("result")[0].style = "font-size: 15px";
+  } else if (calculating.length > 38) {
+    calculating.pop();
+  } else document.getElementsByClassName("result")[0].style = "font-size: 35px";
+};
