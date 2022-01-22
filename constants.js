e18c94b1ad2921d38f8bbe69bdd4845afe6ebc55
KoTuK2306 <dmitriy_aleshin_00@mail.ru>
2022-01-22 18:10:12 +0300

    refactor: constants moved in new file constants.js and refactored code

---
+++ constants.js
@@ -0,0 +1,9 @@
+const constants = {
+  partOfAWhole: "partOfAWhole",
+  square: "square",
+  backspace: "backspace",
+  clean: "clean",
+  uncertainty: "Uncertainty",
+};
+
+export { constants };
--- script.js
+++ script.js
@@ -1,34 +1,39 @@
+import { constants } from "./constants.js";
+
 const [buttonsContainer] = document.getElementsByClassName("buttonsBlock");
 const [result] = document.getElementsByClassName("result");
 let calculating = [];
 
 const buttonClick = (event) => {
   switch (event.target.id) {
-    case "clean":
+    case constants.clean:
       result.innerHTML = 0;
       calculating = [];
       break;
-    case "backspace":
+    case constants.backspace:
       calculating.pop();
       if (calculating.length === 0) {
-        return (result.innerHTML = 0);
+        result.innerHTML = 0;
+        return;
       }
       result.innerHTML = calculating.join("");
       break;
-    case "square":
-      let number = calculating.join("");
+    case constants.square:
+      const number = calculating.join("");
       result.innerHTML = Math.pow(number, 2);
       calculating = [];
       break;
-    case "partOfAWhole":
-      let denominator = calculating.join("");
-      result.innerHTML = 1 / denominator;
+    case constants.partOfAWhole:
+      const denominator = calculating.join("");
+      denominator == 0
+        ? (result.innerHTML = constants.uncertainty)
+        : (result.innerHTML = 1 / denominator);
+      console.log(result.innerHTML);
       calculating = [];
       break;
     default:
       calculating.push(event.target.innerHTML);
       result.innerHTML = calculating.join("");
-      break;
   }
 };
 
