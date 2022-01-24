b75c910296685361afd1730ca78a029b2b051d3e
KoTuK2306 <dmitriy_aleshin_00@mail.ru>
2022-01-24 13:20:39 +0300

    refactor: deleted logic for complex numbers

--- src/utils/sqrt.js
+++ src/utils/sqrt.js
@@ -2,12 +2,7 @@ import { result } from "../constants.js";
 
 export const sqrt = (num) => {
   if (num < 0) {
-    num = Math.sqrt(Math.abs(num));
-    if (Number.isInteger(num) === true) {
-      result.innerHTML = num + "i";
-    } else if ((num - Math.trunc(num)).toString().length > 7) {
-      result.innerHTML = num.toFixed(7) + "i";
-    } else result.innerHTML = num + "i";
+    result.innerHTML = "Error";
   } else {
     num = Math.sqrt(num);
     if (Number.isInteger(num) === true) {
