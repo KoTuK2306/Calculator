143c60d366150ce5c0b52d1cd974d478d4518895
KoTuK2306 <dmitriy_aleshin_00@mail.ru>
2022-01-24 12:42:53 +0300

    feat: improved logic for sqrt button

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 import { operationButtons, buttonsContainer, result } from "./src/constants.js";
 import { sizingText } from "./src/utils/sizingText.js";
+import { sqrt } from "./src/utils/sqrt.js";
 
 let calculating = [];
 
@@ -44,13 +45,7 @@ const buttonClick = (event) => {
       calculating = [];
       break;
     case operationButtons.sqrt:
-      let sqrt = calculating.join("");
-      if (sqrt < 0) {
-        sqrt = Math.sqrt(Math.abs(sqrt));
-        result.innerHTML = sqrt.toFixed(5) + "i";
-        return;
-      }
-      result.innerHTML = Math.sqrt(calculating.join("")).toFixed(5);
+      sqrt(calculating.join(""));
       calculating = [];
       break;
     default:
