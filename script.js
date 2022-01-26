ef2c36d254cc6b989c6cf0968ab6e0decfaf2874
KoTuK2306 <dmitriy_aleshin_00@mail.ru>
2022-01-26 22:41:34 +0300

    feat: the principle of operation of the calculator has been changed

--- script.js
+++ script.js
@@ -1,78 +1,63 @@
-import { operationButtons, buttonsContainer, result } from "./src/constants.js";
+import { operationButtons } from "./src/constants.js";
 import { sizingText } from "./src/utils/sizingText.js";
-import { sqrt } from "./src/utils/sqrt.js";
 
-let calculating = [];
+const [buttonsContainer] = document.getElementsByClassName("buttonsBlock");
+const [result] = document.getElementsByClassName("result");
+
+let str = "";
+let num = 0;
+let sqrtNumberLength = 0;
+let flasher = true;
 
 const buttonClick = (event) => {
   switch (event.target.id) {
     case operationButtons.clean:
+      str = "";
       result.innerHTML = 0;
-      calculating = [];
       break;
     case operationButtons.backspace:
-      calculating.pop();
-      if (calculating.length === 0) {
-        result.innerHTML = 0;
-        return;
-      }
-      result.innerHTML = calculating.join("");
-      break;
-    case operationButtons.square:
-      let number = calculating.join("");
-      number = Math.pow(number, 2);
-      if (number.toString().length > 15) {
-        result.innerHTML = number;
-        result.style = "font-size: 15px";
-        calculating = [];
-        return;
-      }
-      result.innerHTML = number;
-      calculating = [];
-      break;
-    case operationButtons.partOfAWhole:
-      const denominator = calculating.join("");
-      let fraction = 0;
-      denominator == 0
-        ? (result.innerHTML = operationButtons.uncertainty)
-        : (fraction = 1 / denominator);
-      if (fraction.toString().length > 10) {
-        result.innerHTML = fraction.toFixed(10);
-        calculating = [];
-        return;
-      }
-      result.innerHTML = fraction;
-      calculating = [];
-      break;
-    case operationButtons.sqrt:
-      sqrt(calculating.join(""), result);
-      calculating = [];
+      str = str.slice(0, -1);
+      result.innerHTML = str;
+      if (str.length === 0) result.innerHTML = 0;
       break;
     case operationButtons.countUp:
-      const operations = calculating.map((elem) => {
-        if (elem == "x") elem = "*";
-        else if (elem == "÷") elem = "/";
-        else if (elem == ",") elem = ".";
-        return elem;
+      let calc = [];
+      str.split("").map((elem) => {
+        if (elem === "x") {
+          elem = "*";
+          flasher = true;
+        } else if (elem === "÷") {
+          elem = "/";
+          flasher = true;
+        } else if (elem === "√") {
+          elem = "Math.sqrt(";
+          flasher = false;
+        } else if (
+          elem === "+" ||
+          elem === "-" ||
+          elem === "/" ||
+          elem === "*"
+        ) {
+          flasher = true;
+        }
+        if (flasher == false) {
+          sqrtNumberLength = sqrtNumberLength + 1;
+        }
+        calc.push(elem);
+        num = calc.indexOf("Math.sqrt(");
       });
-      result.innerHTML = eval(operations.join(""));
-      calculating = [];
-      result.innerHTML.split("").map((e) => {
-        calculating.push(e);
-      });
-      break;
-    case operationButtons.changeSign:
-      result.innerHTML = calculating.join("") * -1;
-      calculating = [];
-      result.innerHTML.split("").map((e) => {
-        calculating.push(e);
-      });
+      if (sqrtNumberLength !== 0) {
+        calc.splice(num + sqrtNumberLength, 0, ")");
+      }
+      str = eval(calc.join(""));
+      result.innerHTML = str;
+      str = str.toString();
       break;
     default:
-      calculating.push(event.target.innerHTML);
-      result.innerHTML = calculating.join("");
+      str += event.target.innerHTML;
+      result.innerHTML = str;
   }
-  sizingText(calculating, result);
+  sizingText(str, result);
 };
 
 buttonsContainer.addEventListener("click", buttonClick);
--- src/utils/sizingText.js
+++ src/utils/sizingText.js
@@ -1,6 +1,7 @@
 export const sizingText = (calculating, result) => {
   if (calculating.length > 15 && calculating.length <= 38)
     result.style = "font-size: 15px";
-  else if (calculating.length > 38) calculating.pop();
+  else if (calculating.length > 38) calculating.slice(0, -1);
   else result.style = "font-size: 35px";
+  return calculating;
 };
